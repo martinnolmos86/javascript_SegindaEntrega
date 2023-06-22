d69f45b285aa6660706d8ca73a1c4f32d9cbb0f6
Martin Olmos <martinnolmos@gmail.com>
2023-06-22 11:56:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Creación del archivo 'informacion.js'
+
+// Tu nombre (string)
+const nombre = "Martin Olmos";
+
+// Tu edad (number)
+const edad = 37;
+
+// ¿Eres desarrollador? (boolean)
+const esDesarrollador = true;
+
+// fecha de nacimiento (Date)
+const fechaNacimiento = new Date(1986, 5, 28);
+
+// Tu libro favorito
+const libroFavorito = {
+  titulo: "Diario Ole",
+  autor: "No se ",
+  fecha: new Date(2007, 2, 27),
+  url: "https://whttps://www.ole.com.ar/ww.ejemplodelibro.com",
+};
+
+// Creación de la lista como un array
+const lista = [nombre, edad, esDesarrollador, fechaNacimiento, libroFavorito];
+console.log(lista);
